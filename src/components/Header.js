3e682efc22dcfc96f3bc50861d7164284de46a68
venkatesh <venkatesh@ladderstack.com>
2021-05-21 13:46:41 +0530

    made changes in CSS file to match actual HTML

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
       <div className="overlay"></div>
 
       <div className="header-aera">
-        <div className="container-fluid">
+        <div className="container">
           <div className="row">
             <div className="col-lg-12">
               <div className="header-fx">
